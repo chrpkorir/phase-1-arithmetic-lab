138fbd83df5917b959968140241bf97bf842717a
[chrpkorir] <faythkoko@gmail.com>
2024-11-30 15:52:22 +0300

    Worked with arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+//const random = Math.floor(math.random())
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 6
+let num4 = 10
+let mod = num4 % num3
+
+let max = (5, 4 , 16, 20)
